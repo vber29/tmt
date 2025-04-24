b500b3e7cfb61f420f59b075966f017d1d0000be
Martin Hoyer <mhoyer@redhat.com>
2025-04-24 13:27:11 +0200

    Giving up on tmt tests

---
+++ tests/prepare/feature/crb/test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+. ../../../images.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "PROVISION_HOW=${PROVISION_HOW:-container}"
+
+        build_container_image "centos/stream9/upstream\:latest"
+        build_container_image "ubi/8/upstream\:latest"
+
+        rlRun "pushd data"
+    rlPhaseEnd
+
+    images="$TEST_IMAGE_PREFIX/centos/stream9/upstream:latest $TEST_IMAGE_PREFIX/ubi/8/upstream:latest ubi9"
+
+    # EPEL
+    for image in $images; do
+
+        rlPhaseStartTest "Enable EPEL on $image"
+            rlRun -s "tmt -vvv run -a plan --name '/crb/enabled' provision --how container --image $image"
+        rlPhaseEnd
+
+        rlPhaseStartTest "Disable EPEL on $image"
+            rlRun -s "tmt -vvv run -a plan --name '/crb/disabled' provision --how container --image $image"
+        rlPhaseEnd
+
+        if is_centos_stream_9 "$image"; then
+            rlPhaseStartTest "Check CRB on $image"
+                rlRun -s "tmt -vvv run -a plan --name '/crb_package' provision --how container --image $image"
+            rlPhaseEnd
+        fi
+    done
+
+    # Environment profiles
+    # TODO: chicken and egg: we need profile to test whether tmt can apply it, and we need tmt
+    # with support for profiles so we could test profiles and start shipping them...
+    # Once we get the tmt, we can continue with profiles and eventually enable the test below.
+    #
+    # rlPhaseStartTest "Enable EPEL on $image"
+    #     rlRun -s "tmt -vvv run -a plan --name '/profile' provision --how container --image fedora"
+    # rlPhaseEnd
+
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+    rlPhaseEnd
+rlJournalEnd
